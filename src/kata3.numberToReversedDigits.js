ba41ee2bfa06efe47265ab5769493295eb643bae
Jennifer Cant <jcant6@gmail.com>
2020-10-30 12:49:23 +0000

    katas 1 - 3 complete

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,11 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    let array = number.toString().split("").reverse();
+    console.log(array);
+    return array.map(function(x) {
+        return Number(x);
+        console.log(array);
+    });
+    console.log(array);
+};
 
 module.exports = numberToReversedDigits;
